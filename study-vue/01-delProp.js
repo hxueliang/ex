d7973a2bedb8bb605d78fa53fb0d9fe421e5f455
huangxueliang <huangxueliang@lbdfun.com>
2021-02-10 21:19:02 +0800

    feat: 遍历对象实现所有属性响应式

--- study-vue/01-delProp.js
+++ study-vue/01-delProp.js
@@ -1,4 +1,4 @@
-let obj = {}
+// let obj = {}
 
 function defineReactive(obj, key, val) {
   Object.defineProperty(obj, key, {
@@ -8,13 +8,36 @@ function defineReactive(obj, key, val) {
     },
     set(newVale) {
       if (newVale !== val) {
-        console.log(`set -> ${key}:${val}`)
+        console.log(`set -> ${key}:${newVale}`)
         val = newVale
       }
     }
   })
 }
 
-defineReactive(obj, 'foo', 'foo1')
+// defineReactive(obj, 'foo', 'foo1')
+// obj.foo
+// obj.foo = 'fooooooooo'
+
+function observe(obj) {
+  if (typeof obj !== 'object' || typeof obj === null) {
+    return
+  }
+  Object.keys(obj).forEach(key => {
+    defineReactive(obj, key, obj[key])
+  })
+}
+
+let obj = {
+  foo: 'foo',
+  bar: 'bar',
+  baz: {
+    a: 1
+  }
+}
+observe(obj)
 obj.foo
-obj.foo = 'fooooooooo'
+obj.foo = 'foooooooo'
+obj.bar
+obj.bar = 'barrrrrrr'
+obj.baz.a = 1 // 不是响应式数据
